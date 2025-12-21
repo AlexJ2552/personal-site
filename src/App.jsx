2352012c772e1b1dd35eca0bbf47e441edcc9867
Alex Joyce <alexj2552@gmail.com>
2025-12-21 17:01:40 +0000

    Test2 as first one didn't seem to work

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Alex’s Personal Site</h1>
       <h1>Vite + React</h1>
+      <h1>Welcome to my site!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
